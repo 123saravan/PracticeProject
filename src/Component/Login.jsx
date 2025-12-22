c76235ab564196d28a16d90a9bb8e13bacb0c57f
123saravan <sravansravan6055@gmail.com>
2025-12-22 13:00:16 +0530

    Login.jsx

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,5 +1,37 @@
+import React, { useState } from "react";
+import  "../Style/Login.css";
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === "admin@gmail.com" && password === "12345") {
 
-const Login=()=>{
-    return <div>Login Component</div>
+      
+      setMsg("Login Successful");
+    } else {
+      setMsg("Invalid Credentials");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+      <p>{msg}</p>
+    </form>
+  );
 }
+
+export default Login;
